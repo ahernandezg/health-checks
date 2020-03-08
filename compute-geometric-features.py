d1cdaad7a304d9bc269e2f106e100af0475976b0
Andres Hernandez <33495427+andreshernandezg@users.noreply.github.com>
2020-03-08 09:30:42 -0600

    Added print_info() function
    Added print_info() function

--- compute-geometric-features.py
+++ compute-geometric-features.py
@@ -1,6 +1,12 @@
 
 import numpy as np
 
+def print_info():
+    """Print info to user"""
+    print("""Hello, this script helps you compute the area,volume 
+             and perimeter of a rectangle""")
+    return None
+
 
 def compute_area(length, width):
     """Compute area of a square/rectangle"""
