3623bf763a6c36833ff68681769333ce1f2e4017
A Hernandez G <andres.hernandezg@udem.edu>
2020-03-08 10:18:24 -0600

    Added menu() function

--- compute-geometric-features.py
+++ compute-geometric-features.py
@@ -3,9 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
+def menu():
+    "Show a operations menu"
+    print("1......compute area")
+    print("2......compute volume")
+    print("3......compute perimeter")
+    choice = input("Choose an option:")
+    return choice
+
 def print_info():
     """Print info to user"""
-    print("""Hello, this script helps you compute the area,volume 
+    print("""Hello, this script helps you compute the area,volume
              and perimeter of a rectangle""")
     return None
 
@@ -14,9 +22,9 @@ def compute_perimeter(length, width):
     return 2*length+2*width
 
 
-def compute_volume(area, height):
+def compute_volume(length, width, height):
     """"Compute volume of rectangle"""
-    return area*height
+    return length*width*height
 
 
 def compute_area(length, width):
@@ -29,15 +37,19 @@ def main():
     length = 2.0
     width = 1.0
 
-    area = compute_area(length, width)
-    print("Area: ", area)
+    choice = menu()
+    if choice in '1':
+        area = compute_area(length, width)
+        print("Area: ", area)
 
-    height = 2.0
-    volume = compute_volume(area, height)
-    print("Volume:", volume)
+    elif choice in '2':
+        height = 2.0
+        volume = compute_volume(length, width, height)
+        print("Volume:", volume)
 
-    perimeter = compute_perimeter(length, width)
-    print("Perimeter:", perimeter)
+    elif choice in '3':
+        perimeter = compute_perimeter(length, width)
+        print("Perimeter:", perimeter)
 
     return None
 
